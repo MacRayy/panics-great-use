d8cf7d07518630ed29218624143316308cef75e6
Zsolt Makrai <mecrai@gmail.com>
2024-06-27 23:31:20 +0200

    feat: add color const

--- src/app/_components/layout/styles.tsx
+++ src/app/_components/layout/styles.tsx
@@ -1,6 +1,18 @@
 import styled from '@emotion/styled'
+import { color } from '@/app/_constants/color'
 
 export const LayoutContainer = styled.div(() => ({
   display: 'flex',
   flexDirection: 'column',
+}))
+
+export const Header = styled.header(() => ({
+  padding: '1.5rem',
+  backgroundColor: color.primary,
+}))
+
+export const Footer = styled.footer(() => ({
+  padding: '3rem 1.5rem',
+  backgroundColor: color.tertiary,
+  color: color.offWhite,
 }))
---
+++ src/app/_constants/color.ts
@@ -0,0 +1,13 @@
+export const color = {
+  primary: '#FED503',
+  secondary: '#E3000B',
+  tertiary: '#1F1D47',
+  success: '#28a745',
+  danger: '#dc3545',
+  warning: '#ffc107',
+
+  white: '#ffffff',
+  offWhite: '#f8f9fa',
+  gray: '#343a40',
+  black: '#000000',
+}
