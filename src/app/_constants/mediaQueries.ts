5f8484aa6b316fc2b0ba9ca9490ed2cd894b5d00
Zsolt Makrai <mecrai@gmail.com>
2024-07-01 13:35:09 +0200

    feat: create mediaQueries

---
+++ src/app/_constants/mediaQueries.ts
@@ -0,0 +1,6 @@
+export const mediaQueries = {
+  mobileL: '@media (min-width: 425px)',
+  tablet: '@media (min-width: 768px)',
+  laptop: '@media (min-width: 1024px)',
+  laptopL: '@media (min-width: 14400px)',
+}
